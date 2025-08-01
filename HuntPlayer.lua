a0c03a1b8c32fa05842385b0adffa51b178e067f
Bumfuzzle <iinotaku@pizdec.xyz>
2025-08-02 00:12:10 +0300

    Update and rename HuntPlayer.luau to HuntPlayer.lua

--- HuntPlayer.lua
+++ HuntPlayer.lua
@@ -1,9 +1,12 @@
 local player = game:GetService("Players").LocalPlayer
 local screengui = player.PlayerGui:WaitForChild("ScreenGui")
 local Combat_FirstPunch = false
+local alive = false
 player.CharacterAdded:Connect(function(character : Model)
 	local hrp = character:WaitForChild("HumanoidRootPart")
+		alive = true
 	character:WaitForChild("Humanoid").Died:Connect(function()
+				alive = false
 		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer({ "Respawn" })
 		spawn(function()
 			task.wait(4)
@@ -12,9 +15,7 @@ player.CharacterAdded:Connect(function(character : Model)
 			player.PlayerGui:WaitForChild("ScreenGui").Enabled = true
 		end)
 	end)
-end)
-
-	while task.wait(0.5) do
+	while alive do
 		if game.Players[_G.HuntPlayer].Character:WaitForChild("HumanoidRootPart") and player.Character then
 			if not Combat_FirstPunch then
 				Combat_FirstPunch = true
@@ -29,4 +30,8 @@ end)
 				end
 			end
 		end
+			task.wait(0.5)
 	end
+end)
+
+
